c07ac9a2ccac4e63074e65cb7f47ada307648a62
Park jihyun <thotemily@naver.com>
2025-07-06 16:27:35 +0900

    초기 파일

---
+++ src/main/java/org/example/portiony_backend/PortionyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.portiony_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortionyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortionyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/portiony_backend/PortionyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.portiony_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortionyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
