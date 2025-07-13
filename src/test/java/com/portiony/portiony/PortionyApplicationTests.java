52778fffd3daf02a7065828aacd5e14ab7a02609
yeessonng <dorlek856@gmail.com>
2025-07-13 22:12:15 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/portiony/portiony/PortionyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portiony.portiony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortionyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
