ad88f303827d0658a2a3d2e718fb25224c4a7ea3
chaenii25 <chaeun9325@naver.com>
2025-07-18 10:33:16 +0900

    refactor: PostLikeRepository 수정

--- src/main/java/com/portiony/portiony/repository/PostLikeRepository.java
+++ src/main/java/com/portiony/portiony/repository/PostLikeRepository.java
@@ -5,7 +5,7 @@ import com.portiony.portiony.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-//찜 기능 구현 시 사용
+// 찜 내역 조회에 사용
 public interface PostLikeRepository extends JpaRepository<PostLike, Long>, JpaSpecificationExecutor<PostLike> {
     boolean existsByUserAndPostId(User user, Long postId);
 }
