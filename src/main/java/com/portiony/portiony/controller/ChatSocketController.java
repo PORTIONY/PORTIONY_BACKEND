18b1af5c74880456905ca2dd3289313f9b634f06
yeessonng <dorlek856@gmail.com>
2025-07-18 22:40:25 +0900

    refactor: controller 코드 보완

--- src/main/java/com/portiony/portiony/controller/ChatSocketController.java
+++ src/main/java/com/portiony/portiony/controller/ChatSocketController.java
@@ -5,6 +5,7 @@ import com.portiony.portiony.dto.ChatResponseDTO;
 import com.portiony.portiony.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -16,7 +17,7 @@ public class ChatSocketController {
     private final ChatService chatService;
 
     @MessageMapping("/chat/message") // 클라이언트는 /pub/chat/message 로 메시지 전송
-    public void sendMessage(ChatRequestDTO.ChatMessageDTO messageDTO) {
+    public void sendMessage(@Payload ChatRequestDTO.ChatMessageDTO messageDTO) {
         ChatResponseDTO.ChatMessageRsDTO saved = chatService.saveMessage(messageDTO);
         messagingTemplate.convertAndSend(
                 "/sub/chat/room/" + messageDTO.getChatRoomId(), saved);
