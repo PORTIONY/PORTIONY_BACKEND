7340db1f7efe1bdea9f69460a29a7b0ee8bec4b0
yeessonng <dorlek856@gmail.com>
2025-07-20 22:05:12 +0900

    refactor: 코드 리펙토링

--- src/main/java/com/portiony/portiony/controller/ChatSocketController.java
+++ src/main/java/com/portiony/portiony/controller/ChatSocketController.java
@@ -16,9 +16,11 @@ public class ChatSocketController {
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatService chatService;
 
-    @MessageMapping("/chat/message") // 클라이언트는 /pub/chat/message 로 메시지 전송
+    @MessageMapping("/chat/message") // 클라이언트는 stomp /pub/chat/message 로 메시지 전송, 프론트에서 보내는 주소
     public void sendMessage(@Payload ChatRequestDTO.ChatMessageDTO messageDTO) {
+        //db 저장
         ChatResponseDTO.ChatMessageRsDTO saved = chatService.saveMessage(messageDTO);
+        //상대방에게 메시지 전달, /sub/chat/room/{chatRoomId}로 메시지 전송
         messagingTemplate.convertAndSend(
                 "/sub/chat/room/" + messageDTO.getChatRoomId(), saved);
     }
