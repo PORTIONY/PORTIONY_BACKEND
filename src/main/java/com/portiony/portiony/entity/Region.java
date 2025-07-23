5795aa753ee104b6419aecdc3fa8f464416ca222
Lee Hyeon Seung <arale3488@gmail.com>
2025-07-23 17:27:11 +0900

    지역 엔티티 및 csv 수정

--- src/main/java/com/portiony/portiony/entity/Region.java
+++ src/main/java/com/portiony/portiony/entity/Region.java
@@ -14,6 +14,6 @@ public class Region {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 15)
+    @Column(nullable = false, length = 100)
     private String city;
 }
