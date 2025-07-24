9f49b625c6ba766ab41b5e94cc3836b51329f2ca
Lee Hyeon Seung <arale3488@gmail.com>
2025-07-24 15:42:05 +0900

    지역id 수동저장

--- src/main/java/com/portiony/portiony/service/LocationService.java
+++ src/main/java/com/portiony/portiony/service/LocationService.java
@@ -26,6 +26,8 @@ public class LocationService {
     private String kakaoApiKey;
 
     public LocationResponseDto resolveRegionIds(double lat, double lng) {
+        // TODO: 카카오 API로부터 위도/경도를 통해 행정구역 정보 받아오기
+        /*
         String url = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=" + lng + "&y=" + lat;
 
         HttpHeaders headers = new HttpHeaders();
@@ -60,5 +62,10 @@ public class LocationService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 동 없음"));
 
         return new LocationResponseDto(region.getId(), subregion.getId(), dong.getId());
+        */
+
+        // 임시 하드코딩된 지역 ID 반환
+        // TODO: 추후 위 주석 복구하고 실시간 위치 기반 ID 반환 로직 활성화
+        return new LocationResponseDto(22L, 287L, 7486L);
     }
 }
