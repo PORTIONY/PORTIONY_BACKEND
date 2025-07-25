4b5149450a4690929662b5236e458aa904a9068c
Lee Hyeon Seung <arale3488@gmail.com>
2025-07-26 01:14:10 +0900

    상품상세, 공구완료/재개시 연동

--- src/main/java/com/portiony/portiony/dto/Post/UpdatePostStatusResponse.java
+++ src/main/java/com/portiony/portiony/dto/Post/UpdatePostStatusResponse.java
@@ -1,9 +1,24 @@
 package com.portiony.portiony.dto.Post;
 
 import lombok.Getter;
+import com.portiony.portiony.entity.enums.PostStatus;
+
 
 @Getter
 public class UpdatePostStatusResponse {
-    String message = "공구가 완료되었습니다.";
-    String status = "공구 완료";
+    private final String message;
+    private final String status;
+
+    public UpdatePostStatusResponse(PostStatus postStatus) {
+        if (postStatus == PostStatus.DONE) {
+            this.message = "공구가 완료되었습니다.";
+            this.status = "공구 완료";
+        } else if (postStatus == PostStatus.PROGRESS) {
+            this.message = "공구를 재개시하였습니다.";
+            this.status = "공구 중";
+        } else {
+            this.message = "상태 변경됨";
+            this.status = postStatus.name(); // "CANCELLED" 등
+        }
+    }
 }
