d253c4a49db8139fd2f871c8972dc24d55dc5d2b
Lee Hyeon Seung <arale3488@gmail.com>
2025-07-26 17:55:16 +0900

    댓글 조회, 페이지네이션

--- src/main/java/com/portiony/portiony/dto/Post/CreatePostRequest.java
+++ src/main/java/com/portiony/portiony/dto/Post/CreatePostRequest.java
@@ -14,7 +14,7 @@ public class CreatePostRequest {
     private Integer capacity;
     private Integer price;
     private String unit;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    //@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
     private LocalDateTime deadline;
     private String deliveryMethod;
     private Boolean isAgree;
